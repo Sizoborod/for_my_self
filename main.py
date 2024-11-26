8e62a9be6869ecdf0282fcac7a0a976f16f7632b
Dima <grmgorer@gmail.com>
2024-11-26 16:46:09 +0300

    сделал часть работы по SQl

--- main.py
+++ main.py
@@ -54,9 +54,28 @@ def zadacha2():
     def oper1():
         '''Как делать простой запрос на SQL
         ГДима'''
-        pass
-
-
+        #Основной командой для получения какой-либо информации из БД является команда SELECT
+        """SELECT перечень_полей FROM имя_таблицы
+        WHERE условие"""
+        # Пример запроса
+        """SELECT * FROM Films
+    WHERE year = 2010"""
+        # Запрос для двух БД
+        """SELECT title FROM Films 
+                WHERE genre=(
+                        SELECT id FROM genres 
+                            WHERE title = 'фантастика')"""
+        # Использование IN
+        """SELECT title, duration FROM Films 
+                WHERE duration IN (45, 90)""" # IN строго 45 или 90 минут
+        # BETWEEN диапазон(включая границы)
+        """SELECT * FROM Films
+    WHERE (year > 2005) AND duration BETWEEN 45 AND 60 """
+        # Оператор LIKE
+        """SELECT * FROM Films
+    WHERE title like 'А_к%'"""
+        # % — обозначает любое количество, в том числе нулевое, любых символов
+        # _  обозначает один любой символ
 def zadacha3():
     def oper1():
         '''Как делать сложный запрос по нескольким таблицам запрос на SQL
