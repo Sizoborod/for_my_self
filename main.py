acc9852cde624f89825719bff9d1327d68be1a82
sizoborod <se_t610@bk.ru>
2024-11-26 16:16:27 +0300

    Первый коммит

---
+++ main.py
@@ -0,0 +1,24 @@
+def zadacha1():
+    def oper1():
+        '''Открыть CSV с помощью списка'''
+        pass
+
+    def oper2():
+        '''Открыть CSV с помощью словаря'''
+        pass
+
+    def oper3():
+        '''Сохранить CSV с помощью списка'''
+        pass
+
+    def oper4():
+        '''Сохранить CSV с помощью словаря'''
+        pass
+
+
+def zadacha2():
+    pass
+
+
+def zadacha3():
+    pass
